331705591ed192d878c28a36f4c2b89f74867285
Alexander <orientstar96@gmail.com>
2023-02-07 21:52:55 +0500

    3.3 Удаляет в тесте no-debugging-utils

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable testing-library/no-debugging-utils */
 import { render, screen, fireEvent, waitForElementToBeRemoved } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
